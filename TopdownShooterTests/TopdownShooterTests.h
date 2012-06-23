daed1faca6bea266da9cd315abac30879477af4a
Test Mac <musnit@Tests-Mac.local>
2012-06-23 05:43:14 -0700

    Initial Commit

---
+++ TopdownShooterTests/TopdownShooterTests.h
@@ -0,0 +1,13 @@
+//
+//  TopdownShooterTests.h
+//  TopdownShooterTests
+//
+//  Created by Test Mac on 6/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TopdownShooterTests : SenTestCase
+
+@end
---
+++ TopdownShooterTests/TopdownShooterTests.m
@@ -0,0 +1,32 @@
+//
+//  TopdownShooterTests.m
+//  TopdownShooterTests
+//
+//  Created by Test Mac on 6/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "TopdownShooterTests.h"
+
+@implementation TopdownShooterTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TopdownShooterTests");
+}
+
+@end
